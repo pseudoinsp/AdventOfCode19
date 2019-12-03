2ebb14a6cd8efcc1dc75f4c3f493de5feff5eb8c
eberki <gonosztaviszorny@gmail.com>
2019-12-03 21:52:34 +0100

    Day 2 part 2

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -8,27 +8,44 @@ def readInputByLines():
 
     return operations
 
+def calculateProgramResult(operations):
+    currentOpIndex = 0
+
+    while True:
+        if(operations[currentOpIndex] == 99):
+            break
+
+        operand = operations[currentOpIndex]
+        firstOpIndex = operations[currentOpIndex + 1]
+        secondOpIndex = operations[currentOpIndex + 2]
+        targetIndex = operations[currentOpIndex + 3]
+
+        if(operand == 1):
+            operations[targetIndex] = operations[firstOpIndex] + operations[secondOpIndex]
+        elif(operand == 2):
+            operations[targetIndex] = operations[firstOpIndex] * operations[secondOpIndex]
+
+        currentOpIndex += 4
+
+    return operations[0]
+
+def searchParametersForOutput(operations, maxValueofVariablesExclusive, searchedOutput):
+    
+    for currentNoun in range(maxValueofVariablesExclusive):
+        for currentVerb in range(maxValueofVariablesExclusive):
+            opCopy = operations.copy()
+            opCopy[1] = currentNoun
+            opCopy[2] = currentVerb
+
+            if(calculateProgramResult(opCopy) == searchedOutput):
+                return opCopy[1], opCopy[2]
+
+    return -1, -1
+
+
 operations = readInputByLines()
 
-operations[1] = 12;
-operations[2] = 2;
+noun, verb = searchParametersForOutput(operations, 100, 19690720)
 
-currentOpIndex = 0
-while True:
-    if(operations[currentOpIndex] == 99):
-        break
-
-    operand = operations[currentOpIndex]
-    firstOpIndex = operations[currentOpIndex + 1]
-    secondOpIndex = operations[currentOpIndex + 2]
-    targetIndex = operations[currentOpIndex + 3]
-
-    if(operand == 1):
-        operations[targetIndex] = operations[firstOpIndex] + operations[secondOpIndex]
-    elif(operand == 2):
-        operations[targetIndex] = operations[firstOpIndex] * operations[secondOpIndex]
-
-    currentOpIndex += 4
-
-print(f"Part 1: first number in operations: {operations[0]}");
+print(f"Part 2: 100*noun + verb: {100*noun+verb}");
 
